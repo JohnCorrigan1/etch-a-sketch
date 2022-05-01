76e0284bf8d091ee43ca73bd805ccababfcb3ae4
John C <jcorrigan322@gmail.com>
2022-05-01 11:47:10 -0400

    added some basic style, reset and rgb mode buttons

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const grid = document.getElementById('container');
 
-let gg = document.querySelector('button');
-gg.addEventListener('click', function() {
-    getDivs(256);
-});
-
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
 
 
 
@@ -34,9 +37,12 @@ function getDivs(n) {
 };
 
 let slider = document.getElementById('slider');
+const sliderVal = document.getElementById('slider-value');
+
 
 slider.addEventListener('input', function(){
     let val = document.getElementById('slider').value;
+    sliderVal.textContent = val;
     removeCells();
     grid.setAttribute('style', `grid-template-columns: repeat(${val}, 2fr); grid-template-rows: repeat(${val}, 2fr);`);
     for (i = 0; i < val * val; i++) {
@@ -51,3 +57,33 @@ slider.addEventListener('input', function(){
     }
 });
 
+let reset = document.getElementById('reset');
+reset.addEventListener('click', function(){
+    removeCells();
+    let val = document.getElementById('slider').value;
+    grid.setAttribute('style', `grid-template-columns: repeat(${val}, 2fr); grid-template-rows: repeat(${val}, 2fr);`);
+    for (i = 0; i < val * val; i++) {
+        let gridDiv = document.createElement('div');
+        gridDiv.classList.add('gridDiv');
+
+        gridDiv.addEventListener('mouseover', function(event){
+            event.target.style.backgroundColor = 'black';
+        });
+        container.appendChild(gridDiv);
+    }
+});
+
+let rgb = document.getElementById('rgb');
+rgb.addEventListener('click', function(){
+    let val = document.getElementById('slider').value;
+    let cell = grid.children;
+    for(let i = 0; i < val * val; i++) {
+        cell[i].addEventListener('mouseover', function(event){
+            event.target.style.backgroundColor = getRandomColor();
+        })
+    }
+  
+});
+
+
+getDivs(256);
